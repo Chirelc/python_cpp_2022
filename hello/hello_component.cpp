5d3e6b286478e88ca404d8056cd937088569a570
Chirelc <chirelcohen@instance-bc.europe-west1-b.c.projetblockchain.internal>
2022-05-10 09:15:58 +0000

    construction d'un struct

--- hello/hello_component.cpp
+++ hello/hello_component.cpp
@@ -1,22 +1,25 @@
 #include <pybind11/pybind11.h>
 
-char version[]="1.0";
-
-char const* getVersion() {
-	return version;
+struct Version {
+std::string version;
+Version(const std::string &version) : version(version) { }
+void setVersion(const std::string &version_) { version = version_; }
+const std::string  &getVersion()const {
+ return version;
 }
 
-char const* greet() {
+/*char const* greet() {
   return "hello, world";
-}
-
+}*/
+};
 namespace py = pybind11;
 
 
-PYBIND11_MODULE(hello_component,greetings)
+PYBIND11_MODULE(hello_component,m)
 {
-  greetings.doc() = "greeting_object 1.0";
-  
-  greetings.def("greet", &greet, "a function saying hello");
-  greetings.def("getVersion", &getVersion, "a function returning the version");
+py::class_<Version>(m, "Version")
+	.def(py::init<const std::string &>())  
+	.def("setVersion", &Version::setVersion)
+        .def("getVersion", &Version::getVersion);
+	/*def(great,&great,a function saying hello);*/
 }
